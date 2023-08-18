810c4b04bc242f252f5fe18bc86a5bf1d7c9cd91
seri1234 <abc_x_y_z_12@yahoo.co.jp>
2023-08-18 18:22:51 +0900

    微修正

--- components/layout.js
+++ components/layout.js
@@ -1,35 +1,20 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
+const name = '技術ブログ';
+export const siteTitle = 'Next.js and golang blog';
 
 export default function Layout({ children, home }) {
   return (
     <div >
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <div className={styles.container}>
       <header className={styles.site_header} >
       <div className={styles.site_header_wrapper}>
         <Link className={styles.home} href="/">
-          Home
+          HOME
         </Link>
         <nav>
           <ul className={styles.nav_wrapper}>
